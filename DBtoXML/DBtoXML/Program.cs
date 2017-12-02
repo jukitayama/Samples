3873cc3e8ff90b1e48e909ff477014d7f3788e23
jukitayama <subaddress.jk110@gmail.com>
2017-12-02 23:14:21 +0900

    サンプルコードの追加
    [C#][SQLite][LINQ to XML] DBから取得したデータをXMLで返し、受け取ったXMLを解析する
    上記記事のコードを追加

---
+++ DBtoXML/DBtoXML/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DBtoXML
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            // XMLストリームを取得
+            string xmlStream = SQLiteIF.GetStations( "dbファイルのフルパスを指定" );
+
+            try
+            {
+                 // XMLストリームからXElementオブジェクトを作成する場合は、
+                // LoadではなくParseを使うこと。
+                XElement stationsElement = XElement.Parse( xmlStream );
+
+                // <record>要素のコレクションを取得
+                IEnumerable<XElement> stations = stationsElement.Elements( "record" );
+                // <record>に属している要素から、必要な情報を取得する
+                foreach ( var stationElement in stations )
+                {
+                    Console.WriteLine( string.Format( "駅名: {0}", stationElement.Element( "stationName" ).Value ) );
+                    Console.WriteLine( string.Format( "緯度: {0}", stationElement.Element( "latitude" ).Value ) );
+                    Console.WriteLine( string.Format( "経度: {0}", stationElement.Element( "longitude" ).Value ) );
+                    Console.WriteLine();
+                }
+           }
+            catch ( Exception ex )
+            {
+                Console.WriteLine( ex.Message);
+            }
+        }
+    }
+}
---
+++ DBtoXML/DBtoXML/SQLiteIF.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SQLite;
+using System.Xml.Linq;
+
+namespace DBtoXML
+{
+    public class SQLiteIF
+    {
+        /// <summary>
+        /// 駅情報一覧をXML形式で取得
+        /// </summary>
+        /// <param name="dbFilePath">.dbファイルのフルパス</param>
+        static public string GetStations( string dbFilePath )
+        {
+            // 取得したレコードをListで保持します。
+            // 取得したデータはすべてstring型とする。
+            List<Dictionary<string, string>> stationRecords = new List<Dictionary<string, string>>();
+
+            // DBに接続して、クエリを実行する。
+            using ( var sqliteConn = new SQLiteConnection( "Data Source=" + dbFilePath ) )
+            {
+                sqliteConn.Open();
+                using ( SQLiteCommand command = sqliteConn.CreateCommand() )
+                {
+                    // クエリ実行
+                    command.CommandText = "SELECT * from station";
+                    using ( SQLiteDataReader reader = command.ExecuteReader() )
+                    {
+                        // 一行一行読み込んでいく
+                        while ( reader.Read() )
+                        {
+                            // Listに追加
+                            Dictionary<string, string> record = new Dictionary<string, string>() {
+                                { "stationName", reader["station_name"].ToString() },
+                                { "longitude", reader["lon"].ToString() },
+                                { "latitude",  reader["lat"].ToString()}
+                            };
+                            stationRecords.Add( record );
+                        }
+                    }
+                }
+                sqliteConn.Close();
+            }
+
+            // ルート部分のエレメントオブジェクト作成
+            XElement rootElement = new XElement( "records" );
+            // ルートに対して子のエレメントを追加していく
+            foreach ( var record in stationRecords )
+            {
+                XElement el = new XElement( "record",
+                    from keyValue in record
+                    select new XElement( keyValue.Key, keyValue.Value )
+                    );
+                rootElement.Add( el );
+            }
+            return rootElement.ToString();
+        }
+    }
+}
