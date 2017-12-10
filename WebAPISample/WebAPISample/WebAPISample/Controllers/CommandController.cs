74c8b643a4ec0de0141cd7e8cdf5f221338f7fbf
jukitayama <subaddress.jk110@gmail.com>
2017-12-10 20:56:43 +0900

    test

---
+++ WebAPISample/WebAPISample/WebAPISample/Controllers/CommandController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Newtonsoft.Json.Linq;
+
+namespace WebAPISample.Controllers
+{
+    [RoutePrefix("api/Command")]
+    public class CommandController : ApiController
+    {
+        public IHttpActionResult Post( JObject bodyObject )
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebAPISample/WebAPISample/WebAPISample/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace WebAPISample.Controllers
+{
+    [RoutePrefix( "api/Values" )]
+    public class ValuesController : ApiController
+    {
+        public object Get()
+        {
+            List<Dictionary<string, string>> list = new List<Dictionary<string, string>>();
+
+            Dictionary<string, string> dic1 = new Dictionary<string, string>()
+            {
+                {"name", "aaa" },
+                {"id", "1"}
+            };
+            list.Add( dic1 );
+
+            Dictionary<string, string> dic2 = new Dictionary<string, string>()
+            {
+                {"name", "bbb" },
+                {"id", "2"}
+            };
+            list.Add( dic2 );
+            //string jsonStream = "{[name: \"aaa\", id: 1],[name: \"bbb\", id: 2]}";
+            string jsonStream = JsonConvert.SerializeObject( list );
+            JArray jsonObject = JArray.Parse( jsonStream );
+            return jsonObject.ToString();
+        }
+    }
+}
